100638f4f7e12e1bf0454b4dcac6042c6ff2765e
Aarthi0413 <Aarthi.Murugesan@gwcdata.ai>
2024-09-13 15:59:12 +0530

    changed index.js

--- index.js
+++ index.js
@@ -6,29 +6,18 @@ const connectDB = require("./config/db");
 const router = require("./routes/index");
 
 const app = express();
-
-// Define CORS options
-const corsOptions = {
-  origin: function (origin, callback) {
-    // List of allowed origins
-    const allowedOrigins = [process.env.FRONTEND_URL, 'http://localhost:3000', 'http://localhost:3001'];
-    
-    if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
+app.use(cors(
+  {
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  }
+));
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api", router);
 
-console.log("front end url", process.env.FRONTEND_URL);
+console.log("front end url", process.env.FRONTEND_URL)
 
 connectDB()
   .then(() => {
