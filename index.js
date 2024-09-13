f79234eb7c1a49af1ced8767db51f900a13c1c30
Aarthi0413 <Aarthi.Murugesan@gwcdata.ai>
2024-09-13 15:33:20 +0530

    allow multiple origin

--- index.js
+++ index.js
@@ -1,38 +1,3 @@
-// const express = require("express");
-// const cors = require("cors");
-// const cookieParser = require('cookie-parser');
-// require("dotenv").config();
-// const connectDB = require("./config/db");
-// const router = require("./routes/index");
-
-// const app = express();
-// app.use(cors(
-//   {
-//     origin: process.env.FRONTEND_URL,
-//     credentials: true,
-//   }
-// ));
-// app.use(express.json());
-// app.use(cookieParser());
-
-// app.use("/api", router);
-
-// console.log("front end url", process.env.FRONTEND_URL)
-
-// connectDB()
-//   .then(() => {
-//     console.log("MongoDB connected");
-//   })
-//   .catch((err) => {
-//     console.error("Error connecting to MongoDB:", err.message);
-//   });
-
-// const PORT = process.env.PORT || 3000;
-
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
-
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require('cookie-parser');
@@ -45,7 +10,10 @@ const app = express();
 // Define CORS options
 const corsOptions = {
   origin: function (origin, callback) {
-    if (origin === process.env.FRONTEND_URL || origin === 'http://localhost:3000' || origin === 'http://localhost:3001') {
+    // List of allowed origins
+    const allowedOrigins = [process.env.FRONTEND_URL, 'http://localhost:3000', 'http://localhost:3001'];
+    
+    if (allowedOrigins.includes(origin) || !origin) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
