95576fb8a4dbd903d41729404065bcb9b2dbfd1c
Aarthi0413 <Aarthi.Murugesan@gwcdata.ai>
2024-09-13 10:10:00 +0530

    added deployed frontend url

--- controller/getProduct.js
+++ controller/getProduct.js
@@ -4,10 +4,12 @@ const getProduct = async (req, res) => {
   try {
     const product = await Product.find().sort({createdAt:-1});
     res.json({ message: "All products", error: false, success: true, data: product });
+    console.log("get product", product)
   } catch (error) {
     res
       .status(400)
       .json({ message: error.message, error: true, success: false });
+      console.log("Error in get product", error)
   }
 };
 
