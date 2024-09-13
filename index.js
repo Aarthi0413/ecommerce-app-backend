e03df0746e53713208a6d1809e6c06ca1db66e0b
Aarthi0413 <Aarthi.Murugesan@gwcdata.ai>
2024-09-13 15:21:23 +0530

    allow multiple origin

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
+// const express = require("express");
+// const cors = require("cors");
+// const cookieParser = require('cookie-parser');
+// require("dotenv").config();
+// const connectDB = require("./config/db");
+// const router = require("./routes/index");
+
+// const app = express();
+// app.use(cors(
+//   {
+//     origin: process.env.FRONTEND_URL,
+//     credentials: true,
+//   }
+// ));
+// app.use(express.json());
+// app.use(cookieParser());
+
+// app.use("/api", router);
+
+// console.log("front end url", process.env.FRONTEND_URL)
+
+// connectDB()
+//   .then(() => {
+//     console.log("MongoDB connected");
+//   })
+//   .catch((err) => {
+//     console.error("Error connecting to MongoDB:", err.message);
+//   });
+
+// const PORT = process.env.PORT || 3000;
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require('cookie-parser');
@@ -6,18 +41,26 @@ const connectDB = require("./config/db");
 const router = require("./routes/index");
 
 const app = express();
-app.use(cors(
-  {
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-  }
-));
+
+// Define CORS options
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (origin === process.env.FRONTEND_URL || origin === 'http://localhost:3000' || origin === 'http://localhost:3001') {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api", router);
 
-console.log("front end url", process.env.FRONTEND_URL)
+console.log("front end url", process.env.FRONTEND_URL);
 
 connectDB()
   .then(() => {
